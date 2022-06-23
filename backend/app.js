172cce957aee9dd0e83e7c8e4f85b6045e189e8d
David Chung <dvchung00@gmail.com>
2022-06-23 10:14:59 -0700

    Express server setup complete

---
+++ backend/app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+const routes = require('./routes');
+
+const app = express();
+
+// logs information about requests and responses
+app.use(morgan('dev'));
+
+// express.json parses JSON bodies of requests with 'Content-Type' of 'application/json'
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+  })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
+// Connect all the routes
+app.use(routes);
+
+
+
+
+
+
+
+
+
+module.exports = app;
