81bc58d95eb48b6aea919f68abc49d6a0e33dd01
David Chung <dvchung00@gmail.com>
2022-06-29 21:34:48 -0700

    Edited Home page component to only get some questions

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { thunkGetSomeQuestions } from "../../store/questions";
-import { NavLink, Switch, Route } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 
 import './HomePage.css';
 
