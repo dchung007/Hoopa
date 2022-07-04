88b49a18e24645c25350c0de16700d6b952d93fc
David Chung <dvchung00@gmail.com>
2022-07-03 20:29:04 -0700

    Styling and centering the buttons and form titles

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -5,22 +5,26 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <div className="footer-logos">
-        <a href="https://github.com/dchung007">
-          <img className="logo" src={githubLogo} />
-        </a>
+    <div >
+      <div className='footer'>
+        <div className="footer-logos">
+          <a href="https://github.com/dchung007">
+            <img className="logo" src={githubLogo} />
+          </a>
+        </div>
+        <div className="footer-aboutme">
+          <NavLink to="/about">About Me</NavLink>
+        </div>
+        <div className="footer-logos" id="footer-logo-linkedIn">
+          <a href="https://www.linkedin.com/in/david-chung-98a5651aa/">
+            <img className="logo" src={linkedinLogo} />
+          </a>
+        </div>
       </div>
-      <div className="footer-aboutme">
-        <NavLink to="/about">About Me</NavLink>
-      </div>
-      <div className="footer-logos" id="footer-logo-linkedIn">
-        <a href="https://www.linkedin.com/in/david-chung-98a5651aa/">
-          <img className="logo" src={linkedinLogo} />
-        </a>
+      <div className='author-div'>
+        <div> Created by David Chung</div>
       </div>
     </div>
-
   );
 }
 
